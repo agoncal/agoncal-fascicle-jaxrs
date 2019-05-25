72aa17438b90177472bf3c9b42547253e23a71b6
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-05-25 11:19:20 +0200

    Swagger

---
+++ understanding/src/main/java/org/agoncal/fascicle/jaxrs/understanding/AuthorResource.java
@@ -0,0 +1,54 @@
+package org.agoncal.fascicle.jaxrs.understanding;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/authors")
+@Produces(MediaType.TEXT_PLAIN)
+@OpenAPIDefinition(
+  info = @Info(
+    title = "Authors",
+    version = "1.0",
+    description = "Operations on authors",
+    license = @License(name = "Apache 2.0", url = "http://cdbookstore.com/license"),
+    contact = @Contact(url = "http://cdbookstore.com/contact", name = "Antonio", email = "agoncal.fascicle@gmail.com")
+  ),
+  tags = {
+    @Tag(name = "author"),
+    @Tag(name = "book")
+  }
+)
+public class AuthorResource {
+
+  String[] scifiAuthors = {"Isaac Asimov", "Ray Bradbury", "Douglas Adams"};
+
+  @GET
+  @Operation(summary = "Gets all the sci-fi authors", tags = {"scifi"})
+  public String getAllScifiAuthors() {
+    return String.join(", ", scifiAuthors);
+  }
+
+  @GET
+  @Path("/{index}")
+  @Operation(summary = "Gets a sci-fi author by index",
+    tags = {"scifi"},
+    responses = {
+      @ApiResponse(responseCode = "400", description = "Invalid index supplied"),
+      @ApiResponse(responseCode = "404", description = "Author not found")}
+  )
+  public String getScifiAuthor(@Parameter(description = "Author index", required = true) @PathParam("index") int index) {
+    return scifiAuthors[index];
+  }
+}
---
+++ understanding/src/test/java/org/agoncal/fascicle/jaxrs/understanding/AuthorResourceTest.java
@@ -0,0 +1,45 @@
+package org.agoncal.fascicle.jaxrs.understanding;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class AuthorResourceTest extends JerseyTest {
+
+  @Override
+  protected Application configure() {
+    return new ResourceConfig(AuthorResource.class);
+  }
+
+  // ======================================
+  // =              Methods               =
+  // ======================================
+
+  @Test
+  public void shouldGetAllAuthors() {
+    String response = target("/authors").request().get(String.class);
+    assertEquals("Isaac Asimov, Ray Bradbury, Douglas Adams", response);
+  }
+
+  @Test
+  public void shouldGetAuthor() {
+    String response = target("/authors/0").request().get(String.class);
+    assertEquals("Isaac Asimov", response);
+  }
+
+  @Test
+  public void shouldNotFindResource() {
+    Response response = target("/dummy").request().get();
+    assertEquals(404, response.getStatus());
+  }
+}
